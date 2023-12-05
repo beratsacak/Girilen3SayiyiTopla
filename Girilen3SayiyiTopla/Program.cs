678848f54db604454b7d48328cf4936d69d72558
berat584 <berat584@BPC08>
2023-12-05 09:30:05 +0300

    Proje dosyası ekle.

---
+++ Girilen3SayiyiTopla/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Girilen3SayiyiTopla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            İslem islem= new İslem();
+            int toplam =islem.toplama(sayi2:10,sayi3:50,sayi1:40);
+            Console.WriteLine(toplam);
+        }
+    }
+    public class İslem
+    {
+        public int toplama (int sayi1, int sayi2,int sayi3)
+        {
+            return (sayi1+sayi2+sayi3);
+        }
+    }
+}
